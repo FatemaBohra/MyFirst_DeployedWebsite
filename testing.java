6eb083d22c16a78d1cdb91876c61b5ad91b7e08e
Fatema Bohra <fatema.bohra2003@gmail.com>
2023-03-22 05:41:46 -0300

    Create testing.java
    Prints even values till the user counts finishes.

---
+++ testing.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+
+        int even = 0;
+        System.out.println(even);
+
+        int x = 1;
+        while(x < count){
+            even = even + 2;
+            System.out.println(even);
+            x++;
+        }
+    }
+}
